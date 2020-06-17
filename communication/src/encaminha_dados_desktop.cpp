ec59408ba77bc2e41c277f169df2142f45132c26
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2020-06-17 19:34:46 -0300

    tentando ajustar zmq

--- communication/src/encaminha_dados_desktop.cpp
+++ communication/src/encaminha_dados_desktop.cpp
@@ -33,11 +33,11 @@ using namespace zmq;
 typedef PointXYZRGB PointT;
 
 context_t ctx{1};
-socket_t *im_sender;
-socket_t *cl_sender;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 void imagemCallback(const sensor_msgs::ImageConstPtr& msg){
+    socket_t im_sender(ctx, ZMQ_PUB);
+    im_sender.bind("tcp://*:5557");
     // Converte mensagem
     cv_bridge::CvImagePtr im = cv_bridge::toCvCopy(msg, msg->encoding);
     // Cria objeto do Protobuf
@@ -63,10 +63,13 @@ void imagemCallback(const sensor_msgs::ImageConstPtr& msg){
     img_proto.SerializeToString(&buffer_imagem);
     message_t img_zmq(buffer_imagem.length());
     memcpy(img_zmq.data(), buffer_imagem.data(), buffer_imagem.length());
-    im_sender->send(img_zmq);
+    im_sender.send(img_zmq);
+    im_sender.close();
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg){
+    socket_t cl_sender(ctx, ZMQ_PUB);
+    cl_sender.bind("tcp://*:5558");
     // Converter mensagem
     PointCloud<PointT>::Ptr cloud (new PointCloud<PointT>);
     fromROSMsg(*msg, *cloud);
@@ -88,19 +91,14 @@ void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg){
     cloud_proto.SerializeToString(&buffer_nuvem);
     message_t nuvem_zmq(buffer_nuvem.length());
     memcpy(nuvem_zmq.data(), buffer_nuvem.data(), buffer_nuvem.length());
-    cl_sender->send(nuvem_zmq);
+    cl_sender.send(nuvem_zmq);
+    cl_sender.close();
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "encaminha_dados_desktop");
     ros::NodeHandle nh;
-
-    // Inicia os sockets zmq
-    im_sender = new socket_t(ctx, ZMQ_PUB);
-    im_sender->bind("tcp://*:5557");
-    cl_sender = new socket_t(ctx, ZMQ_PUB);
-    cl_sender->bind("tcp://*:5558");
 
     // Subscriber para topicos de imagem e de nuvem
     ros::Subscriber im_sub = nh.subscribe("/imagem_obj", 100, imagemCallback);
