0c5f5314ecacc036c8b771cca9d3929299bb7ba4
vinicius <vinicius.vidal@engenharia.ufjf.br>
2020-11-24 12:20:17 -0300

    notificando da aquisicao em si e da camera funcionando

--- communication/src/system_state.cpp
+++ communication/src/system_state.cpp
@@ -2,12 +2,17 @@
 #include "communication/state.h"
 #include "sensor_msgs/Image.h"
 #include "std_msgs/Bool.h"
+#include "std_msgs/String.h"
 
 using namespace std;
 
-bool state_global = false;
+bool state_cam_global = false, state_scn_global = false;
 
 void camCallback(const sensor_msgs::ImageConstPtr& msg){
+
+}
+
+void feedbackCallback(const std_msgs::StringConstPtr& msg){
 
 }
 
@@ -15,10 +20,10 @@ bool switch_state(communication::state::Request &req, communication::state::Resp
     // Avaliando mudanca de sistema dita na entrada
     switch(req.state){
     case 0:
-//        state_global = false;
+//        state_cam_global = false;
         res.result = 1;
     case 1:
-//        state_global = true;
+//        state_cam_global = true;
         res.result = 1;
     default:
         res.result = 0;
@@ -33,16 +38,21 @@ int main(int argc, char **argv)
     ROS_INFO("Ligando monitoramento do sistema.");
 
     ros::Subscriber    sub_cam = nh.subscribe("/camera/image_raw", 10, camCallback);
+    ros::Subscriber    sub_scn = nh.subscribe("/feedback_scan", 10, feedbackCallback);
     ros::ServiceServer service = nh.advertiseService("switch_state", switch_state);
-    ros::Publisher     pub     = nh.advertise<std_msgs::Bool>("system_state", 10);
+    ros::Publisher     pub_cam = nh.advertise<std_msgs::Bool>("camera_state", 10);
+    ros::Publisher     pub_scn = nh.advertise<std_msgs::Bool>("scan_state"  , 10);
 
     ros::Rate r(2);
+    std_msgs::Bool msg;
     while(ros::ok()){
-        std_msgs::Bool msg;
-        msg.data = state_global;
-        pub.publish(msg);
+        msg.data = state_cam_global;
+        pub_cam.publish(msg);
+        msg.data = state_scn_global;
+        pub_scn.publish(msg);
 
-        state_global = (sub_cam.getNumPublishers() == 0) ? false : true;
+        state_cam_global = (sub_cam.getNumPublishers() == 0) ? false : true;
+        state_scn_global = (sub_scn.getNumPublishers() == 0) ? false : true;
 
         r.sleep();
         ros::spinOnce();
