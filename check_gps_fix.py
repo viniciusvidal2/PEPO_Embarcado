683e2cf0efa9fbfc474aad3ad7a04d563c61c8df
vinicius <vinicius.vidal@engenharia.ufjf.br>
2021-04-21 09:42:41 -0300

    testando feedback gps

---
+++ check_gps_fix.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import time
+import serial
+import adafruit_gps
+
+def check():
+    # Serial Port
+    uart = serial.Serial("/dev/ttyTHS1", baudrate=9600, timeout=10)
+
+    # Create a GPS module instance.
+    gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
+
+    # Turn on the basic GGA and RMC info (what you typically want)
+    gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+
+    # Set update rate to once a second (1hz) which is what you typically want.
+    gps.send_command(b"PMTK220,1000")
+
+    # Acquire from GPS
+    time.sleep(0.5)
+    gps.update()
+
+    # Check fix
+    fix = 'yes' if gps.has_fix else 'no'
+
+    uart.close()
+
+    return fix
